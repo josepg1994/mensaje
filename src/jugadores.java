a0776c434fb185c895c336358f9a913b6541e1a6
josepg1994 <yepaa17@gmail.com>
2018-01-28 12:53:37 +0100

    Commit inicial

---
+++ src/jugadores.java
@@ -0,0 +1,14 @@
+package dados;
+
+public class jugadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Bienvenido al sistema");
+		System.out.print("Hoy es lunes");
+		System.out.print("Son las 8 de la mañana");
+		System.out.print("Tiene 8 correos sin leer");
+
+	}
+
+}
